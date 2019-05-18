e19c2f76315c816be64275566089011bcafb502b
Grant Horner <h.grant.horner@gmail.com>
2019-05-18 14:22:26 -0400

    Fungi now grow automatically

---
+++ src/Entity/Entities/MyPlayer.ts
@@ -0,0 +1,46 @@
+import { Entity } from '../Entity'
+import { IEntityProperties, IMap } from '../../@types'
+import { Actor, Mover } from '../Helpers'
+import { Attacker } from '../Helpers/Attacker'
+import { Destructible } from '../Helpers/Destructible'
+
+export class MyPlayer extends Entity {
+  readonly _mover: Mover = new Mover
+  readonly actor: Actor = new Actor
+  readonly attacker: Attacker = new Attacker(1)
+  readonly defenses: Destructible = new Destructible(10)
+
+  constructor(props: IEntityProperties) {
+    super(props)
+  }
+
+  attack = <T extends Entity>(target: T) => {
+    this.attacker.attack(target)
+  }
+
+  takeDamage = (damage: number) => {
+    const newHP = this.defenses.takeDamage(damage)
+    if (newHP <= 0 && this.map) {
+      this.map.removeEntity(this)
+    }
+  }
+
+  tryMove = (x: number, y: number, map: IMap) => {
+    const res = this._mover.tryMove(x, y, map)
+
+    if (res.target) {
+      console.log('there is a target')
+      this.attack(res.target)
+      res.target.takeDamage(this.attacker.attackPower)
+    } else if (res.success) {
+      this.x = res.x
+      this.y = res.y
+    }
+  }
+
+  act = () => {
+    return this.actor.act()
+  }
+}
+
+
